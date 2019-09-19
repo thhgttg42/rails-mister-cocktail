f9b4e55ebc1a46646042e3346a97fc8e4fc3c243
Andre Lauritzen <lauritzen.42@gmail.com>
2019-09-19 14:58:33 +0200

    Front-End - Mister Cocktail - respec spec/models, all green.

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, :ingredient_id, :cocktail_id, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
