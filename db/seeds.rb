179eb95b50239830133e235441931df2866c2304
Andre Lauritzen <lauritzen.42@gmail.com>
2019-09-19 15:36:09 +0200

    Front-End - Mister Cocktail - Just did the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "dry vermouth")
+Ingredient.create(name: "port")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "coffee")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "cream")
+Ingredient.create(name: "coffee liqueur")
+console.log(seeded)
